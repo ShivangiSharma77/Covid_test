99d8052d6f55e0c9ff7f87bbe70a8ec766d104b3
Shivangi Sharma <sshivangi77ssharma@gmail.com>
2020-05-17 11:47:58 +0530

    first commit from local

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_blog import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    # password= db.Column(db.String(60))
+
+    def __repr__(self):
+        return ("User is - %s , Email is - %s " %(self.username, self.email))
+        
