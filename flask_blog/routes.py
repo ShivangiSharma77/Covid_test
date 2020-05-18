53ccfb31c227b3dbe65ff5a74dfbe0302d48f45b
Shivangi Sharma <sshivangi77ssharma@gmail.com>
2020-05-18 08:59:27 +0530

    Adding file system

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -4,6 +4,8 @@ from flask import render_template,url_for,flash,redirect
 from flask_blog import app, db  
 from flask_blog.forms import RegistrationForm, LoginForm, RForm
 from flask_blog.models import User
+import cv2
+import glob
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8) 
@@ -24,8 +26,11 @@ def home():
         user = User(username=form.username.data, email=form.email.data)
         db.session.add(user)
         db.session.commit()
-        flash('Information Submitted', category='success')
+        flash('Information Submitted', category='success')       
         return redirect(url_for('result'))
+        # num =1
+        # return "res"+ str(val(num))
+
     return render_template('home.html', title= 'Covid-19',form=form)
     
 
@@ -48,4 +53,26 @@ def login():
 
 @app.route('/result')
 def result():
-    return render_template('result.html', title= 'results')
+    num= "hello"
+    s=val(num)
+    return render_template('result.html', title= 'results', s=s)
+
+# @app.route('/ <int:num>')
+# def result(num=1):
+#     return "your code <hr> res" + str(val(num))
+#     # return render_template('result.html', title= 'results')
+
+def val(num):
+    imdir = 'flask_blog/static/pro/'
+    ext = ['png', 'jpg', 'gif']    # Add image formats here
+
+    files = []
+    [files.extend(glob.glob(imdir + '*.' + e)) for e in ext]
+
+
+    images = [cv2.imread(file) for file in files]
+
+    cv2.imshow('title',images[-1])
+    cv2.waitKey(5000)
+    cv2.destroyAllWindows()
+    return ( str(num))
